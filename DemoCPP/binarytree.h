6fea1e858cc1331681fa64fc964a26a3fa80e21b
Antonio Soto <fantoniosoto@gmail.com>
2022-08-13 01:24:40 -0500

    insert function with parent and branch

--- DemoCPP/binarytree.h
+++ DemoCPP/binarytree.h
@@ -11,23 +11,21 @@ using namespace std;
 template <typename T>
 class NodeBinaryTree
 {
-public:
-  typedef T         Type;
-private:
-  typedef NodeBinaryTree<T> Node;
+  public:
+    typedef T         Type;
+  private:
+    typedef NodeBinaryTree<T> Node;
   public:
     T       m_data;
-    Node *  m_pParent = nullptr;
+    Node    *m_pParent = nullptr;
     vector<Node *> m_pChild = {nullptr, nullptr}; // 2 hijos inicializados en nullptr
     bool    m_visitedFlag = false;
-    Node* m_pParent = nullptr;
   public:
     NodeBinaryTree(Node *pParent, T data, Node *p0 = nullptr, Node *p1 = nullptr) 
         : m_pParent(pParent), m_data(data)
     {   m_pChild[0] = p0;   m_pChild[1] = p1;   }
     T         getData()                {   return m_data;    }
     T        &getDataRef()             {   return m_data;    }
-    // void      setpChild(Node *pChild, size_t pos)  {   m_pChild[pos] = pChild;  }
     Node    * getChild(size_t branch){ return m_pChild[branch];  }
     Node    *&getChildRef(size_t branch){ return m_pChild[branch];  }
     Node    * getParent() { return m_pParent;   }
@@ -35,7 +33,8 @@ private:
 
 template <typename Container>
 class binary_tree_iterator : public general_iterator<Container,  class binary_tree_iterator<Container> > // 
-{public:   
+{
+  public:   
     typedef class general_iterator<Container, binary_tree_iterator<Container> > Parent; 
     typedef typename Container::Node                                  Node; // 
     typedef binary_tree_iterator<Container>                               myself;
@@ -45,20 +44,11 @@ class binary_tree_iterator : public general_iterator<Container,  class binary_tr
     binary_tree_iterator(myself &other)  : Parent (other) {}
     binary_tree_iterator(myself &&other) : Parent(other) {} // Move constructor C++11 en adelante
 
-public:
-    binary_tree_iterator operator++() {
-      Parent::m_pNode -> changeVisitedFlag();
-      Parent::m_pNode = getNext(Parent::m_pNode);
+  public:
+    backward_iterator operator++() {
       return *this;
     }
-protected:
-    Node *getNext(Node* &rCurrent) {
-      Node* possibleNext = rCurrent -> getChildRef(1);
-      if (possibleNext && possibleNext -> getVisitedFlag() != true)  return Parent::m_pContainer -> getMostLeft(possibleNext);
-      Node* parent = rCurrent -> getParentRef();
-      if (parent -> getVisitedFlag() != true) return parent;
-      return getNext(parent);
-    }
+
 };
 
 template <typename _T>
@@ -88,32 +78,44 @@ class BinaryTree
     typedef BinaryTree<Traits>              myself;
     typedef binary_tree_iterator<myself>    iterator;
 
-protected:
+  protected:
     Node    *m_pRoot = nullptr;
     size_t   m_size  = 0;
     CompareFn Compfn;
-public: 
+  public: 
     size_t  size()  const       { return m_size;       }
     bool    empty() const       { return size() == 0;  }
-    void    insert(value_type &elem) { internal_insert1(elem, nullptr, m_pRoot);  }
+    void    insert(value_type &elem) { first_insert(elem);  }
 
-protected:
+  protected:
     Node *CreateNode(Node *pParent, value_type &elem){ return new Node(pParent, elem); }
-    Node *internal_insert1(value_type &elem, Node *pParent, Node *&rpOrigin)
-    {
-        if( !rpOrigin ) //  llegué al fondo de una rama
-            return (rpOrigin = CreateNode(pParent, elem));
-        size_t branch = Compfn(elem, rpOrigin->getDataRef() );
-        return internal_insert1(elem, rpOrigin, rpOrigin->getChildRef(branch));
+    
+    void first_insert(value_type &elem) {
+      if (!m_pRoot) {
+        m_pRoot = CreateNode(nullptr, elem);
+        return;
+      }
+      size_t branch = Compfn(elem, m_pRoot -> getDataRef());
+      return recursive_insert(elem, m_pRoot, branch);
     }
-public:
+
+    void recursive_insert(value_type &elem, Node *&m_pParent, size_t &branch) {
+      if (!m_pParent -> getChildRef(branch))  {
+        m_pParent -> getChildRef(branch) = CreateNode(m_pParent, elem);
+        return;
+      }
+      size_t newBranch = Compfn(elem, m_pParent -> getChildRef(branch) -> getDataRef());
+      return recursive_insert(elem, m_pParent -> getChildRef(branch), newBranch);
+    }
+
+  public:
     void inorder  (ostream &os)    {   inorder  (m_pRoot, os, 0);   }
     void postorder(ostream &os)    {   postorder(m_pRoot, os, 0); }
     void preorder (ostream &os)    {   preorder (m_pRoot, os, 0);  }
     void inorder(void (*visit) (value_type& item))
     {   inorder(m_pRoot, visit);    }
 
-protected:
+  protected:
     void inorder(Node  *pNode, ostream &os, size_t level)
     {
         if( pNode )
