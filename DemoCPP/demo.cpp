202e7e4535e6e833d6d4c37b1991ce73033dac46
lenninquiso <lenninquiso@gmail.com>
2022-09-10 09:05:49 -0500

    TODO 1 agregar Demobtree al main.cpp

--- DemoCPP/demo.cpp
+++ DemoCPP/demo.cpp
@@ -3051,3 +3051,40 @@ void DemoScope()
 }
 
 
+#include "btree.h"
+const char * keys1 = "D1XJ2xTg8zKL9AhijOPQcEowRSp0NbW567BUfCqrs4FdtYZakHIuvGV3eMylmn";
+const char * keys2 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const char * keys3 = "DYZakHIUwxVJ203ejOP9Qc8AdtuEop1XvTRghSNbW567BfiCqrs4FGMyzKLlmn";
+const char * keys4 = "ABC";
+string keys = "";
+const size_t BTreeSize = 3;
+
+void Demobtree()
+{
+    size_t result, i=2, aux=1;
+    //BTree <char> bt (BTreeSize);
+    BTree <char> bt;
+    system("cls");
+    while (i > 1 && aux < 2)
+    { 
+        for( i = 0; keys4[i]; i++)
+        {
+            //cout<<"Inserting "<<keys1[i]<<endl;
+            //result = bt.Insert(keys4[i], i*i);
+            bt.Insert(keys4[i], i*i);
+            //bt.Print(cout);
+        }
+        i++;
+        bt.Print(cout);
+        for( i = 0; keys4[i]; i++)
+        {
+                bt.Remove(keys4[i], i*i);
+        }
+        i++;
+        //cout << i << endl;
+        aux++;
+    
+    }
+}
+
+
